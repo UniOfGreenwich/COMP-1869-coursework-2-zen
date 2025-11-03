8fe65433a68d9b26c0845b0bdcbf4d904722da2a
Joshua Gorton <jg6582v@gre.ac.uk>
2025-11-03 19:57:04 +0000

    added the ring of statues
    full functionality and added game states and manager

--- Zen/Assets/Zen Game Assets/Scripts/statue.cs
+++ Zen/Assets/Zen Game Assets/Scripts/statue.cs
@@ -4,6 +4,7 @@ public class statue : MonoBehaviour
 {
     public bool haveBeenSelected;
     public Vector3 sittingSpot;
+    public bool isMissingStatue;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
