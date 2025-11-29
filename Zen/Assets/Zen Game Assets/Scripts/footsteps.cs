08df8ebe8263224ffe2ef0d3de70afa51301e41c
jack <jj6530x@greenwich.ac.uk>
2025-11-29 15:09:07 +0000

    fixed footsteps
    also added pick up and put down audio to statues, but need coding assistance

--- Zen/Assets/Zen Game Assets/Scripts/footsteps.cs
+++ Zen/Assets/Zen Game Assets/Scripts/footsteps.cs
@@ -7,24 +7,48 @@ public class footsteps : MonoBehaviour
     public Vector3 playervelocity;
     public GameObject triggerPoint;
     public bool OnWood;
+    bool OnWoodcheck;
+    public triggerpoint Tp;
     [SerializeField] AudioClip ground1;
     [SerializeField] AudioClip ground2;
 
     void Update()
     {
         playervelocity = GameObject.FindAnyObjectByType<playerMovment>().velocityInput;
-        OnWood = GameObject.FindAnyObjectByType<triggerpoint>().onWood;
-        if ((playervelocity != Vector3.zero) && (OnWood = false))
+        
+        if (Tp.onWood == true)
         {
-            //footstep.enabled = true;
+            OnWoodcheck = true;
+        }
+        else if (Tp.onWood == false)
+        {
+            OnWoodcheck = false;
+        }
+        if ((playervelocity != Vector3.zero) && (OnWoodcheck == false))
+        {
+            footstep.enabled = true;
             footstep.clip = ground1;
         }
-        else if ((playervelocity != Vector3.zero) && (OnWood = true))
+        else if ((playervelocity == Vector3.zero) && (OnWoodcheck == false))
         {
-            //footstep.enabled = true;
-            footstep.clip = ground2;
+            footstep.enabled = false;
+            footstep.clip = ground1;
         }
         
+         if ((playervelocity != Vector3.zero) && (OnWoodcheck == true))
+        {
+            footstep.enabled = true;
+            footstep.clip = ground2;
+        }
+        if ((playervelocity == Vector3.zero) && (OnWoodcheck == true))
+        {
+            footstep.enabled = false;
+            footstep.clip = ground2;
+        }
+
+        Debug.Log(OnWoodcheck);
+
+
 
 
 
--- Zen/Assets/Zen Game Assets/Scripts/statue.cs
+++ Zen/Assets/Zen Game Assets/Scripts/statue.cs
@@ -5,6 +5,10 @@ public class statue : MonoBehaviour
     public bool haveBeenSelected;
     public Vector3 sittingSpot;
     public bool isMissingStatue;
+    public AudioSource AudioSource;
+    [SerializeField] AudioClip pickUp;
+    [SerializeField] AudioClip putDown;
+    bool missing;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +18,30 @@ public class statue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        missing = GameObject.FindAnyObjectByType<PlayerIntoraction>().inMissing;
+        Pickup();
+        PutDown();
         
+       
     }
+
+    void Pickup()
+    {
+        if (haveBeenSelected == true)
+        {
+            AudioSource.enabled = true;
+            AudioSource.PlayOneShot(pickUp);
+            
+        }
+    }
+
+    void PutDown()
+    {
+        if (missing == false)
+        {
+            AudioSource.PlayOneShot(putDown);
+        }
+    }
+
+
 }
